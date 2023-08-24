4f0dc51eea2c79fd23d74cc2f5e0be47ff7a107d
millky <quality.script@outlook.com>
2023-08-24 23:59:49 +0300

    New AppErrorWebExceptionHandler / Update at SQL + Swagger + New Mapping + New Documentation

--- src/main/java/com/example/socialmediaapi/controllers/PostRestController.java
+++ src/main/java/com/example/socialmediaapi/controllers/PostRestController.java
@@ -7,6 +7,7 @@ import com.example.socialmediaapi.security.CustomPrincipal;
 import com.example.socialmediaapi.service.PostService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -24,7 +25,7 @@ public class PostRestController {
 
     @PostMapping("/create")
     @Operation(summary = "Создание нового Поста / Записи ")
-    public Mono<PostDto> register(@RequestBody PostDto dto, Authentication authentication) {
+    public Mono<PostDto> createPost(@RequestBody PostDto dto, Authentication authentication) {
         PostEntity entity = postMapper.map(dto);
         CustomPrincipal customPrincipal = (CustomPrincipal) authentication.getPrincipal();
 
@@ -32,21 +33,31 @@ public class PostRestController {
                 .map(postMapper::map);
     }
 
-    @Operation(summary = "Получение существующей Записи / Поста ")
+    @Operation(summary = "Получение существующей Записи / Поста",
+    description = "Получение поста происходит по его индивидумальному {id}")
     @GetMapping("/{id}")
     public Mono<PostDto> findById (@PathVariable(value = "id") Long id) {
         return postService.findById(id).map(postMapper::map);
     }
 
-    @Operation(summary = "Редактирование по /{id} существующей Записи / Поста ")
+    @Operation(summary = "Редактирование по {id} существующей Записи / Поста ",
+    description = "Редактирование существующео поста происходит по его {id}." +
+            " Пользователь имеет право редактировать только те посты, которые ему принадлежат." +
+            " В случае если пользователь попытается отредактировать пост, который ему не принадлежит," +
+            " Будет ошибка.")
     @PostMapping("/{id}/update")
     public Mono<Void> updatePost (@RequestBody PostDto dto,
                                   @PathVariable(value = "id") Long id) {
         PostEntity entity = postMapper.map(dto);
         return postService.updatePost(dto.getText(), dto.getTitle(), id);
     }
-    @Operation(summary = "Удаление по /{id} существующей Записи / Поста ")
+    @Operation(summary = "Удаление по /{id} существующей Записи / Поста ",
+    responses = { @ApiResponse (
+            description = "Записи с данным ID не существует, либо она не принадлежит пользователю",
+            responseCode = "500"
+    )})
     @PostMapping("/{id}/delete")
+    @ApiResponse(description = "Запись успешно удалена", responseCode = "200")
     public Mono<Void> deletePost (@PathVariable(value = "id") Long id) {
        return postService.deletePost(id);
     }
